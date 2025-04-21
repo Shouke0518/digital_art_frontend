e19626b7002bf801983a503edf77c0b628866e29
K <shit141414@gmail.com>
2025-04-21 16:13:54 +0800

    adjust host ip and port

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 10000,
+    cors: true,
+  },
   plugins: [
     vue(),
     vueDevTools(),
