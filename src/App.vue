a345b441ef743b32afd7c72b87d65ee80de36b1e
K <shit141414@gmail.com>
2025-05-09 00:19:27 +0800

    add font and muu vue content.

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,14 @@ import { useDisplay } from 'vuetify'
 <template>
   <RouterView></RouterView>
 </template>
+
+<style>
+    @font-face {
+        font-family: 'BoutiqueBitmap9x9';
+        src: url('/fonts/BoutiqueBitmap9x9_1.92.ttf') format('truetype');
+    }
+
+    body {
+        font-family: 'BoutiqueBitmap9x9', monospace;
+    }
+</style>
